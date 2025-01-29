ff5698270dba1946f292c542f2703ba615926efa
mashakrot <makrosensei@gmail.com>
2025-01-29 16:32:23 +0200

    aaaaaaaaaaaaaaaaa

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js" 
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], 
+};
